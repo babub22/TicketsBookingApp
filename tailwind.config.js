b8b8b19dd13beb5050d2090fe71805861c458d96
babub22 <vladlesovoy2@gmail.com>
2023-06-12 13:24:42 +0200

    styles: Add new colors presets to `TailwindCSS` config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}" ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: "rgb(39, 55, 77)",
+        customTeal: "rgb(82, 109, 130)",
+        customGray: "rgb(157, 178, 191)",
+        customLightGray: "rgb(221, 230, 237)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
